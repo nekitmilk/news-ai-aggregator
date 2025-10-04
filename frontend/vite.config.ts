9380ec087371c77db81cdc0f62141ba5ea53c200
Mokrov Danila <mok-danil28@mail.ru>
2025-10-04 04:26:17 +0300

    [NAA] - [NAA] [Frontend] Удалены ненужные компоненты

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,25 @@
-import path from "node:path"
-import { tanstackRouter } from "@tanstack/router-plugin/vite"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from 'node:path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
-})
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/api/v1',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    assetsInlineLimit: 0,
+  },
+  plugins: [react()],
+});
