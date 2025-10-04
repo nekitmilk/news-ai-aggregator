22a871b343980f0056c29908fe5918762d07234d
Mokrov Danila <mok-danil28@mail.ru>
2025-10-04 04:26:17 +0300

    [NAA] - [NAA] [Frontend] Удалены ненужные компоненты

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,49 +1,26 @@
-import {
-  MutationCache,
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
-import { createRouter, RouterProvider } from "@tanstack/react-router"
-import { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
-import { ApiError, OpenAPI } from "./client"
-import { CustomProvider } from "./components/ui/provider"
-import { routeTree } from "./routeTree.gen"
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { OpenAPI } from './client';
+import App from './App';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
-OpenAPI.BASE = import.meta.env.VITE_API_URL
+OpenAPI.BASE = import.meta.env.VITE_API_URL;
 OpenAPI.TOKEN = async () => {
-  return localStorage.getItem("access_token") || ""
-}
+  return localStorage.getItem('access_token') || '';
+};
 
-const handleApiError = (error: Error) => {
-  if (error instanceof ApiError && [401, 403].includes(error.status)) {
-    localStorage.removeItem("access_token")
-    window.location.href = "/login"
-  }
-}
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: handleApiError,
-  }),
-  mutationCache: new MutationCache({
-    onError: handleApiError,
-  }),
-})
+// const handleApiError = (error: Error) => {
+//   if (error instanceof ApiError && [401, 403].includes(error.status)) {
+//     localStorage.removeItem('access_token');
+//     window.location.href = '/login';
+//   }
+// };
 
-const router = createRouter({ routeTree })
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router
-  }
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CustomProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </CustomProvider>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
